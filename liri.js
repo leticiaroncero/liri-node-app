fcc8ef8679a8e763e32ef76e18b6ba309542de20
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-11-24 20:04:03 -0800

    import axios and moment packages

--- liri.js
+++ liri.js
@@ -1,5 +1,24 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
+var axios = require("axios");
+var moment = require("moment");
 
 var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+
+if (command === "concert-this") {
+    var artist = process.argv.slice(3).join(" ");
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            for (var i = 0; i < response.length; i++) {
+                var artistEvent = response[i];
+                var venueName = artistEvent.venue.name;
+                var venueLoc = artistEvent.venue.city + ", " + artistEvent.venue.region + ", " + artistEvent.venue.country;
+                var eventDate = artistEvent.datetime;
+            }
+
+
+        })
+}
+
