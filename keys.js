d47017f25824901938f1c137347cd34aa2d13a4e
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-11-27 18:50:40 -0800

    change bands in town app id and move it to env folder

--- keys.js
+++ keys.js
@@ -2,3 +2,5 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bandsintown = process.env.BANDS_IN_TOWN_ID;
--- liri.js
+++ liri.js
@@ -9,20 +9,20 @@ var command = process.argv[2];
 
 if (command === "concert-this") {
     var artist = process.argv.slice(3).join(" ");
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsintown)
         .then(function (response) {
             if (response.data.length > 0) {
-            for (var i = 0; i < response.data.length; i++) {
-                var artistEvent = response.data[i];
-                var venueName = artistEvent.venue.name;
-                var venueLoc = artistEvent.venue.city + ", " + artistEvent.venue.region + ", " + artistEvent.venue.country;
-                var eventDate = moment(artistEvent.datetime, 'YYYY-MM-DDTHH:mm:ss').format("MM/DD/YYYY");
-                console.log(venueLoc + " at " + venueName + " on " + eventDate);
-            }
+                for (var i = 0; i < response.data.length; i++) {
+                    var artistEvent = response.data[i];
+                    var venueName = artistEvent.venue.name;
+                    var venueLoc = artistEvent.venue.city + ", " + artistEvent.venue.region + ", " + artistEvent.venue.country;
+                    var eventDate = moment(artistEvent.datetime, 'YYYY-MM-DDTHH:mm:ss').format("MM/DD/YYYY");
+                    console.log(venueLoc + " at " + venueName + " on " + eventDate);
+                }
             } else {
                 console.log("No upcoming events for " + artist);
             }
-        }).catch(function(error) {
+        }).catch(function (error) {
             console.log("There was an error trying to find that artist.");
         });
 }
