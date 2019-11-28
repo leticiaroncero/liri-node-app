2b3f4f48a6d0eb7057395a877bc66fd472bb0e27
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-11-27 18:25:56 -0800

    add error handling for bandsintown API

--- liri.js
+++ liri.js
@@ -11,6 +11,7 @@ if (command === "concert-this") {
     var artist = process.argv.slice(3).join(" ");
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
         .then(function (response) {
+            if (response.data.length > 0) {
             for (var i = 0; i < response.data.length; i++) {
                 var artistEvent = response.data[i];
                 var venueName = artistEvent.venue.name;
@@ -18,8 +19,11 @@ if (command === "concert-this") {
                 var eventDate = moment(artistEvent.datetime, 'YYYY-MM-DDTHH:mm:ss').format("MM/DD/YYYY");
                 console.log(venueLoc + " at " + venueName + " on " + eventDate);
             }
-
-
-        })
+            } else {
+                console.log("No upcoming events for " + artist);
+            }
+        }).catch(function(error) {
+            console.log("There was an error trying to find that artist.");
+        });
 }
 
